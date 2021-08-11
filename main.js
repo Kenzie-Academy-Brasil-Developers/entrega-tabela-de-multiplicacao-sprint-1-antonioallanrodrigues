0d5f8a54441893d02cd8a7e2d1b60b9aab4fa413
Antonio Allan <antonioallan@hotmail.com>
2021-08-11 15:45:21 -0300

    final

---
+++ main.js
@@ -0,0 +1,11 @@
+function tabelaDeMultiplicacao(n) {
+    let x = [];
+    for (let i = 0; i <= n; i++) {
+        x[i] = [];
+        for (let j = 0; j <= n; j++) {
+            x[i][j] = i * j
+        }
+    }
+    console.table(x)
+    return x
+}
